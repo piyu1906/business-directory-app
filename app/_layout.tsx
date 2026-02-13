5afc0f8e8c6befe92ffc9a254807da553e556c05
Priyanshi Joshi <priyanshij884@gmail.com>
2026-02-13 19:27:20 +0530

    Refactor layout and home screen components. Added font loading in RootLayout and simplified HomeScreen to display a basic "Home" text. Removed unused imports and components for cleaner code.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,22 +1,39 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { Stack } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
-import 'react-native-reanimated';
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from "@react-navigation/native";
+import { useFonts } from "expo-font";
+import { Stack } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+import "react-native-reanimated";
 
-import { useColorScheme } from '@/hooks/use-color-scheme';
+import { useColorScheme } from "@/hooks/use-color-scheme";
+import { ActivityIndicator } from "react-native";
 
 export const unstable_settings = {
-  anchor: '(tabs)',
+  anchor: "(tabs)",
 };
 
 export default function RootLayout() {
+  const [fontsLoaded] = useFonts({
+    appFont: require("@/assets/fonts/Montserrat-Regular.ttf"),
+    appFontBold: require("@/assets/fonts/Montserrat-Bold.ttf"),
+  });
   const colorScheme = useColorScheme();
 
+  if (!fontsLoaded) {
+    return <ActivityIndicator />;
+  }
+
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+    <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
       <Stack>
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
+        <Stack.Screen
+          name="modal"
+          options={{ presentation: "modal", title: "Modal" }}
+        />
       </Stack>
       <StatusBar style="auto" />
     </ThemeProvider>
